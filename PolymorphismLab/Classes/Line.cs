a0834487ce3a02fceb0f96211cae82f993d593e1
X00109141 <X00109141@computing.stu.it-tallaght.ie>
2016-09-29 12:17:59 +0100

    Adding some work done in the Polymorphism lab

---
+++ PolymorphismLab/Classes/Line.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PolymorhismLab.Classes
+{
+    public class Line : Shape
+    {
+        public Vertex Vertex1 { get; set; }
+        public Vertex Vertex2 { get; set; }
+
+        public override string ToString()
+        {
+            return "Colours: " + Colour + "\nCoordinates: (" + Vertex1.XCoordinate + "," + Vertex1.YCoordinate + ")" + " (" + Vertex2.XCoordinate + "," + Vertex2.YCoordinate + ")";
+        }
+
+        // To be overridden from Shape - to be done
+        public virtual Line Translate(Vertex vertex1, Vertex vertex2, int amountToTranslate)
+        {
+            // Calculation by a single amount passed in
+
+            int vertX1 = vertex1.XCoordinate - amountToTranslate;
+            int vertY1 = vertex1.YCoordinate - amountToTranslate;
+            int vertX2 = vertex2.XCoordinate - amountToTranslate;
+            int vertY2 = vertex2.YCoordinate - amountToTranslate;
+
+            return new Line()
+            {
+                Colour = Colour,
+                Vertex1 = new Vertex
+                {
+                    XCoordinate = vertX1,
+                    YCoordinate = vertY1
+                },
+                Vertex2 = new Vertex
+                {
+                    XCoordinate = vertX2,
+                    YCoordinate = vertY2
+                }
+            };
+        }
+    }
+}
---
+++ PolymorphismLab/Classes/Shape.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PolymorhismLab.Classes
+{
+    public class Shape
+    {
+        public String Colour { get; set; }
+        public override string ToString()
+        {
+            return "Colour: " + Colour;
+        }
+
+        public Vertex Translate(int amountToTranslate, Vertex vertexTranslation)
+        {
+            vertexTranslation.XCoordinate -= amountToTranslate;
+            vertexTranslation.YCoordinate -= amountToTranslate;
+            return vertexTranslation;
+        }
+    }
+}
