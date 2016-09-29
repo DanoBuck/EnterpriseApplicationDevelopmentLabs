f663422c65a9a6c633efc1227124c3c8b6f50dac
X00109141 <X00109141@computing.stu.it-tallaght.ie>
2016-09-29 12:30:02 +0100

    Adding the Circle class to project

---
+++ PolymorphismLab/Classes/Circle.cs
@@ -0,0 +1,31 @@
+﻿using PolymorhismLab.Classes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PolymorphismLab.Classes
+{
+    public class Circle : Shape
+    {
+        public Vertex OriginVertex { get; set; }
+        public double Radius { get; set; }
+
+        public override string ToString()
+        {
+            return "Coordinates: (" + OriginVertex.XCoordinate + "," + OriginVertex.YCoordinate + ")" + "\nColour: " + Colour;
+        }
+
+        public virtual Circle Translate(Vertex vertex, int amountToTranslate)
+        {
+            return new Circle();
+        }
+
+        public double Area(int radius)
+        {
+            double area = Math.PI * radius * radius;
+            return area;
+        }
+    }
+}
