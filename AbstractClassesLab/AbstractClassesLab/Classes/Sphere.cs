740e27ef3660a044f15da6b7bd5483e284b6a164
X00109141 <X00109141@computing.stu.it-tallaght.ie>
2016-09-29 12:13:35 +0100

    Adding completed abstract classes lab

---
+++ AbstractClassesLab/AbstractClassesLab/Classes/Sphere.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace AbstractClassesLab1
+{
+    class Sphere : ThreeDShape
+    {
+        private double radius { get; set; }
+
+        public Sphere(double radiusIn, String correspondIn) : base ()
+        {
+            radius = radiusIn;
+            corresponding = correspondIn;
+        }
+
+        public Sphere() : base("")
+        {
+        }
+
+        // To Be Finished
+        public override double CalculateVolumeOfShape()
+        {
+            double fraction = 1.3333333;
+            double rad = fraction * Math.PI * radius * radius * radius;
+            return rad;
+        }
+
+        public override String ToString()
+        {
+            return "Radius: " + radius + "\nCorresponding: " + corresponding;
+        }
+
+    }
+}
---
+++ AbstractClassesLab/AbstractClassesLab/Classes/ThreeDShape.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractClassesLab1
+{
+    public abstract class ThreeDShape
+    {
+        private String shape { get; set; }
+        public String corresponding { get; set; }
+
+        public ThreeDShape()
+        {
+            shape = "";
+            corresponding = shape;
+        }
+
+        public ThreeDShape(String shapeIn)
+        {
+            shape = shapeIn;
+            corresponding = shapeIn;
+        }
+
+        // Abstract Method
+        public abstract double CalculateVolumeOfShape();
+
+        public override String ToString()
+        {
+            return "Shape: " + shape + "\nCorresponding: " + corresponding;
+        }
+    }
+}
