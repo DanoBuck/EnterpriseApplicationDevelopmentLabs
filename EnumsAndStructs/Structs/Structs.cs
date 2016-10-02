e1762482c7ee8dfee23d264f3d10dba47b696656
DanoBuck <X00109141@gmail.com>
2016-10-02 18:32:18 +0100

    Enums and Structs Lab now complete

--- EnumsAndStructs/Structs/Structs.cs
+++ EnumsAndStructs/Structs/Structs.cs
@@ -9,9 +9,20 @@ namespace EnumsAndStructs.Structs
     public class Structs
     {
         public struct Currencys {
+            // Conversions needed for euros
             public const double conversionRateOfOneEuroInDollars = 1.12;
             public const double conversionRateOfOneEuroToYen = 113.93;
             public const double conversionRateOfOneEuroToPounds = 0.87;
+
+            // Conversions needed for dollars
+            public const double conversionRateOfOneDollarInEuro = .89;
+            public const double conversionRateOfOneDollarToYen = 101.34;
+            public const double conversionRateOfOneDollarToPounds = 0.77;
+
+            // Conversions for pounds
+            public const double conversionRateOfOnePoundInEuro = 1.15;
+            public const double conversionRateOfOnePoundToDollars = 1.30;
+            public const double conversionRateOfOnePoundToYen = 131.52;
 
             public double MoneyAmount { get; set; }
             public string MoneyCurrency { get; set; }
@@ -35,11 +46,130 @@ namespace EnumsAndStructs.Structs
                 return Math.Round(convertedMoney, 1);
             }
 
-            // To Be Implemented
-            public double MoneyAdditionUsingDifferentMoney(Currencys currency1, Currencys currency2)
+            public double ConversionOfMoneyForDollars(string toMoney)
             {
+                double convertedMoney = 0;
 
-                return 0;
+                if (toMoney == "€")
+                {
+                    convertedMoney = MoneyAmount * conversionRateOfOneDollarInEuro;
+                }
+                else if (toMoney == "YEN")
+                {
+                    convertedMoney = MoneyAmount * conversionRateOfOneDollarToYen;
+                }
+                else if (toMoney == "£")
+                {
+                    convertedMoney = MoneyAmount * conversionRateOfOneDollarToPounds;
+                }
+                else // Not Found
+                {
+                    convertedMoney = MoneyAmount;
+                }
+                return Math.Round(convertedMoney, 1);
+            }
+
+            public double ConversionOfMoneyForPounds(string toMoney)
+            {
+                double convertedMoney = 0;
+
+                if (toMoney == "€")
+                {
+                    convertedMoney = MoneyAmount * conversionRateOfOnePoundInEuro;
+                }
+                else if (toMoney == "YEN")
+                {
+                    convertedMoney = MoneyAmount * conversionRateOfOnePoundToYen;
+                }
+                else if (toMoney == "$")
+                {
+                    convertedMoney = MoneyAmount * conversionRateOfOnePoundToDollars;
+                }
+                else // Not Found
+                {
+                    convertedMoney = MoneyAmount;
+                }
+                return Math.Round(convertedMoney, 1);
+            }
+
+            public double MoneyAdditionUsingDifferentMoneyToEuro(Currencys currency1, Currencys currency2)
+            {
+                double conversionReturned = 0;
+                if(currency1.MoneyCurrency == "£" && currency2.MoneyCurrency == "$")
+                {
+                    // Convert currency1 and 2 to euro
+                    double conversion1 = currency1.ConversionOfMoneyForPounds("€");
+                    double conversion2 = currency2.ConversionOfMoneyForDollars("€");
+                    conversionReturned = conversion1 + conversion2;
+                }
+                else if (currency1.MoneyCurrency == "$" && currency2.MoneyCurrency == "£")
+                {
+                    // Convert currency1 and 2 to euro
+                    double conversion1 = currency1.ConversionOfMoneyForDollars("€");
+                    double conversion2 = currency2.ConversionOfMoneyForPounds("€");
+                    conversionReturned = conversion1 + conversion2;
+                } 
+                else if (currency1.MoneyCurrency == "£" && currency2.MoneyCurrency == "£")
+                {
+                    double conversion1 = currency1.ConversionOfMoneyForPounds("€");
+                    double conversion2 = currency2.ConversionOfMoneyForPounds("€");
+                    conversionReturned = conversion1 + conversion2;
+                }
+                else if (currency1.MoneyCurrency == "$" && currency2.MoneyCurrency == "$")
+                {
+                    double conversion1 = currency1.ConversionOfMoneyForDollars("€");
+                    double conversion2 = currency2.ConversionOfMoneyForDollars("€");
+                    conversionReturned = conversion1 + conversion2;
+                }
+                else if(currency1.MoneyCurrency == "€" && currency2.MoneyCurrency == "$")
+                {
+                    double conversion1 = currency1.MoneyAmount;
+                    double conversion2 = currency2.ConversionOfMoneyForDollars("€");
+                    conversionReturned = conversion1 + conversion2;
+                }
+                else
+                {
+                    conversionReturned = 0;
+                }
+
+                return Math.Round(conversionReturned, 1);
+            }
+
+            public double MoneyAdditionUsingDifferentMoneyToDollars(Currencys currency1, Currencys currency2)
+            {
+                double conversionReturned = 0;
+                if (currency1.MoneyCurrency == "€" && currency2.MoneyCurrency == "€")
+                {
+                    // Convert currency1 and 2 to dollars
+                    double conversion1 = currency1.ConversionOfMoneyForEuro("$");
+                    double conversion2 = currency2.ConversionOfMoneyForEuro("$");
+                    conversionReturned = conversion1 + conversion2;
+                }
+                else if (currency1.MoneyCurrency == "£" && currency2.MoneyCurrency == "£")
+                {
+                    // Convert currency1 and 2 to dollars
+                    double conversion1 = currency1.ConversionOfMoneyForPounds("$");
+                    double conversion2 = currency2.ConversionOfMoneyForPounds("$");
+                    conversionReturned = conversion1 + conversion2;
+                }
+                else if (currency1.MoneyCurrency == "£" && currency2.MoneyCurrency == "€")
+                {
+                    double conversion1 = currency1.ConversionOfMoneyForPounds("$");
+                    double conversion2 = currency2.ConversionOfMoneyForEuro("$");
+                    conversionReturned = conversion1 + conversion2;
+                }
+                else if (currency1.MoneyCurrency == "€" && currency2.MoneyCurrency == "£")
+                {
+                    double conversion1 = currency1.ConversionOfMoneyForEuro("$");
+                    double conversion2 = currency2.ConversionOfMoneyForPounds("$");
+                    conversionReturned = conversion1 + conversion2;
+                }
+                else
+                {
+                    conversionReturned = 0;
+                }
+
+                return Math.Round(conversionReturned, 1);
             }
         }
     }
--- UnitTestsForLabs/EnumsAndStructsTests/StructsTests.cs
+++ UnitTestsForLabs/EnumsAndStructsTests/StructsTests.cs
@@ -35,7 +35,203 @@ namespace UnitTestsForLabs.EnumsAndStructsTests
         public void ConversionOfEuroToPounds()
         {
             double conversion = myStruct.ConversionOfMoneyForEuro("£");
-            Assert.AreEqual(conversion, 105.8);
+            Assert.AreEqual(conversion, 106.1);
+        }
+
+        [TestMethod]
+        public void ConversionOfDollarsToEuro()
+        {
+            myStruct.MoneyCurrency = "$";
+            double convserion = myStruct.ConversionOfMoneyForDollars("€");
+            Assert.AreEqual(convserion, 108.6);
+        }
+
+        [TestMethod]
+        public void ConversionOfDollarsToYen()
+        {
+            myStruct.MoneyCurrency = "$";
+            double convserion = myStruct.ConversionOfMoneyForDollars("YEN");
+            Assert.AreEqual(convserion, 12363.5);
+        }
+
+        [TestMethod]
+        public void ConversionOfDollarsToPounds()
+        {
+            myStruct.MoneyCurrency = "$";
+            double convserion = myStruct.ConversionOfMoneyForDollars("£");
+            Assert.AreEqual(convserion, 93.9);
+        }
+
+        [TestMethod]
+        public void ConversionOfPoundsToEuros()
+        {
+            myStruct.MoneyCurrency = "£";
+            double convserion = myStruct.ConversionOfMoneyForPounds("€");
+            Assert.AreEqual(convserion, 140.3);
+        }
+
+        [TestMethod]
+        public void ConversionOfPoundsToDollars()
+        {
+            myStruct.MoneyCurrency = "£";
+            double convserion = myStruct.ConversionOfMoneyForPounds("$");
+            Assert.AreEqual(convserion, 158.6);
+        }
+
+        [TestMethod]
+        public void ConversionOfPoundsToYEN()
+        {
+            myStruct.MoneyCurrency = "£";
+            double convserion = myStruct.ConversionOfMoneyForPounds("YEN");
+            Assert.AreEqual(convserion, 16045.4);
+        }
+
+        [TestMethod]
+        public void MoneyAdditionUsingPoundsToEuroTest()
+        {
+            myStruct = new EnumsAndStructs.Structs.Structs.Currencys()
+            {
+                MoneyAmount = 122.00,
+                MoneyCurrency = "£"
+            };
+
+            double conversionAddition = myStruct.MoneyAdditionUsingDifferentMoneyToEuro(myStruct, myStruct);
+            Assert.AreEqual(conversionAddition, 280.6);
+        }
+
+        [TestMethod]
+        public void MoneyAdditionUsingDollarsToEuroTest()
+        {
+            myStruct = new EnumsAndStructs.Structs.Structs.Currencys()
+            {
+                MoneyAmount = 122.00,
+                MoneyCurrency = "$"
+            };
+
+            double conversionAddition = myStruct.MoneyAdditionUsingDifferentMoneyToEuro(myStruct, myStruct);
+            Assert.AreEqual(conversionAddition, 217.2);
+        }
+
+
+        [TestMethod]
+        public void MoneyAdditionUsingDollarsAndPoundsToEuroTest()
+        {
+            myStruct = new EnumsAndStructs.Structs.Structs.Currencys()
+            {
+                MoneyAmount = 122.00,
+                MoneyCurrency = "$"
+            };
+
+            EnumsAndStructs.Structs.Structs.Currencys myStruct2 = new EnumsAndStructs.Structs.Structs.Currencys()
+            {
+                MoneyAmount = 122.00,
+                MoneyCurrency = "£"
+            };
+
+            double conversionAddition = myStruct.MoneyAdditionUsingDifferentMoneyToEuro(myStruct, myStruct2);
+            Assert.AreEqual(conversionAddition, 248.9);
+        }
+
+        [TestMethod]
+        public void MoneyAdditionUsingEurosAndDollarsToEuroTest()
+        {
+            myStruct = new EnumsAndStructs.Structs.Structs.Currencys()
+            {
+                MoneyAmount = 122.00,
+                MoneyCurrency = "€"
+            };
+
+            EnumsAndStructs.Structs.Structs.Currencys myStruct2 = new EnumsAndStructs.Structs.Structs.Currencys()
+            {
+                MoneyAmount = 122.00,
+                MoneyCurrency = "$"
+            };
+
+            double conversionAddition = myStruct.MoneyAdditionUsingDifferentMoneyToEuro(myStruct, myStruct2);
+            Assert.AreEqual(conversionAddition, 230.6);
+        }
+
+        [TestMethod]
+        public void MoneyAdditionUsingPoundsAndDollarsToEuroTest()
+        {
+            myStruct = new EnumsAndStructs.Structs.Structs.Currencys()
+            {
+                MoneyAmount = 122.00,
+                MoneyCurrency = "£"
+            };
+
+            EnumsAndStructs.Structs.Structs.Currencys myStruct2 = new EnumsAndStructs.Structs.Structs.Currencys()
+            {
+                MoneyAmount = 122.00,
+                MoneyCurrency = "$"
+            };
+
+            double conversionAddition = myStruct.MoneyAdditionUsingDifferentMoneyToEuro(myStruct, myStruct2);
+            Assert.AreEqual(conversionAddition, 248.9);
+        }
+
+        [TestMethod]
+        public void MoneyAdditionUsingEurosToDollarsTest()
+        {
+            myStruct = new EnumsAndStructs.Structs.Structs.Currencys()
+            {
+                MoneyAmount = 122.00,
+                MoneyCurrency = "€"
+            };
+
+            double conversionAddition = myStruct.MoneyAdditionUsingDifferentMoneyToDollars(myStruct, myStruct);
+            Assert.AreEqual(conversionAddition, 273.2);
+        }
+
+        [TestMethod]
+        public void MoneyAdditionUsingPoundsToDollarsTest()
+        {
+            myStruct = new EnumsAndStructs.Structs.Structs.Currencys()
+            {
+                MoneyAmount = 122.00,
+                MoneyCurrency = "£"
+            };
+
+            double conversionAddition = myStruct.MoneyAdditionUsingDifferentMoneyToDollars(myStruct, myStruct);
+            Assert.AreEqual(conversionAddition, 317.2);
+        }
+
+        [TestMethod]
+        public void MoneyAdditionUsingPoundsAndEurosToDollarsTest()
+        {
+            myStruct = new EnumsAndStructs.Structs.Structs.Currencys()
+            {
+                MoneyAmount = 122.00,
+                MoneyCurrency = "£"
+            };
+
+            EnumsAndStructs.Structs.Structs.Currencys myStruct2 = new EnumsAndStructs.Structs.Structs.Currencys()
+            {
+                MoneyAmount = 122.00,
+                MoneyCurrency = "€"
+            };
+
+            double conversionAddition = myStruct.MoneyAdditionUsingDifferentMoneyToDollars(myStruct, myStruct2);
+            Assert.AreEqual(conversionAddition, 295.2);
+        }
+
+        [TestMethod]
+        public void MoneyAdditionUsingEurosAndPoundsToDollarsTest()
+        {
+            myStruct = new EnumsAndStructs.Structs.Structs.Currencys()
+            {
+                MoneyAmount = 122.00,
+                MoneyCurrency = "£"
+            };
+
+            EnumsAndStructs.Structs.Structs.Currencys myStruct2 = new EnumsAndStructs.Structs.Structs.Currencys()
+            {
+                MoneyAmount = 122.00,
+                MoneyCurrency = "€"
+            };
+
+            double conversionAddition = myStruct.MoneyAdditionUsingDifferentMoneyToDollars(myStruct2, myStruct);
+            Assert.AreEqual(conversionAddition, 295.2);
         }
     }
 }
