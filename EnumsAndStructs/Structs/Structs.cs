333a9bd895c9f9b8ac3d71289dc791f34f942f73
DanoBuck <X00109141@gmail.com>
2016-10-02 16:26:02 +0100

    A bit of work done on Structs and Enums, needs to be finished

---
+++ EnumsAndStructs/Structs/Structs.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EnumsAndStructs.Structs
+{
+    public class Structs
+    {
+        public struct Currencys {
+            public const double conversionRateOfOneEuroInDollars = 1.12;
+            public const double conversionRateOfOneEuroToYen = 113.93;
+            public const double conversionRateOfOneEuroToPounds = 0.87;
+
+            public double MoneyAmount { get; set; }
+            public string MoneyCurrency { get; set; }
+
+            public double ConversionOfMoneyForEuro(string toMoney)
+            {
+                double convertedMoney = 0;
+
+                if (toMoney == "$")
+                {
+                    convertedMoney = MoneyAmount * conversionRateOfOneEuroInDollars;
+                }
+                else if (toMoney == "YEN")
+                {
+                    convertedMoney = MoneyAmount * conversionRateOfOneEuroToYen;
+                }
+                else if(toMoney == "£")
+                {
+                    convertedMoney = MoneyAmount * conversionRateOfOneEuroToPounds;
+                }
+                return Math.Round(convertedMoney, 1);
+            }
+
+            // To Be Implemented
+            public double MoneyAdditionUsingDifferentMoney(Currencys currency1, Currencys currency2)
+            {
+
+                return 0;
+            }
+        }
+    }
+}
---
+++ UnitTestsForLabs/EnumsAndStructsTests/StructsTests.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestsForLabs.EnumsAndStructsTests
+{
+    [TestClass]
+    public class StructsTests
+    {
+        EnumsAndStructs.Structs.Structs.Currencys myStruct;
+
+        [TestInitialize]
+        public void SetUp()
+        {
+            myStruct = new EnumsAndStructs.Structs.Structs.Currencys()
+            {
+                MoneyAmount = 122.00,
+                MoneyCurrency = "€"
+            };
+        }
+
+        [TestMethod]
+        public void ConversionOfEuroToDollars()
+        {
+            double conversion = myStruct.ConversionOfMoneyForEuro("$");
+            Assert.AreEqual(conversion, 136.6);
+        }
+
+        [TestMethod]
+        public void ConversionOfEuroToYEN()
+        {
+            double conversion = myStruct.ConversionOfMoneyForEuro("YEN");
+            Assert.AreEqual(conversion, 13899.5);
+        }
+
+        [TestMethod]
+        public void ConversionOfEuroToPounds()
+        {
+            double conversion = myStruct.ConversionOfMoneyForEuro("£");
+            Assert.AreEqual(conversion, 105.8);
+        }
+    }
+}
