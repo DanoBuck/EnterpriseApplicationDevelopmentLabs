59d268868fcaf8e0b5db940e2a7c77e0df51a56e
DanoBuck <X00109141@gmail.com>
2016-10-09 12:11:22 +0100

    Updated solution to use a List<Student>() instead of an ArrayList

---
+++ CollectionsAndGenerics/ClassesUsingListInstead/ListOfStudentsRevised.cs
@@ -0,0 +1,91 @@
+﻿using CollectionsAndGenerics.Classes;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CollectionsAndGenerics.ClassesUsingListInstead
+{
+    public class ListOfStudentsRevised
+    {
+        public int ID { get; set; }
+        public string LecturerName { get; set; }
+        public List<Student> StudentArrayList { get; set; }
+
+        public ListOfStudentsRevised()
+        {
+            ID = 0;
+            LecturerName = "";
+            StudentArrayList = new List<Student>();
+        }
+
+        public bool AddStudentToList(Student newStudent)
+        {
+            bool hasBeenAdded = false;
+
+            if (StudentArrayList.Count == 0)
+            {
+                StudentArrayList.Add(newStudent);
+                hasBeenAdded = true;
+            }
+            else
+            {
+                // Search if ID already exists
+                foreach (Student s in StudentArrayList)
+                {
+                    if (s.StudentID == newStudent.StudentID)
+                    {
+                        hasBeenAdded = false;
+                        throw new Exception("This StudentID already exists!");
+                    }
+                }
+                // Add the new student
+                StudentArrayList.Add(newStudent);
+                hasBeenAdded = true;
+            }
+            return hasBeenAdded;
+        }
+
+        // Indexers
+        public Student this[int i]
+        {
+            get
+            {
+                if (i > StudentArrayList.Count)
+                {
+                    throw new IndexOutOfRangeException("Index out of bounds!");
+                }
+                else
+                {
+                    // Cast the Student that is found
+                    return (Student)StudentArrayList[i];
+                }
+            }
+        }
+
+        public Student this[string studentId]
+        {
+            get
+            {
+                bool hasBeenFound = false;
+                Student foundStudent = null;
+                foreach (Student sa in StudentArrayList)
+                {
+                    if (sa.StudentID == studentId)
+                    {
+                        hasBeenFound = true;
+                        foundStudent = sa;
+                    }
+                }
+                // Throw an exception if the student is not found
+                if (hasBeenFound == false)
+                {
+                    throw new IndexOutOfRangeException("Student not found!");
+                }
+                return foundStudent;
+            }
+        }
+    }
+}
---
+++ UnitTestsForLabs/CollectionsAndGenericsTests/ListTests/ListTests.cs
@@ -0,0 +1,122 @@
+﻿using CollectionsAndGenerics.Classes;
+using CollectionsAndGenerics.ClassesUsingListInstead;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+
+namespace UnitTestsForLabs.CollectionsAndGenericsTests.ListTests
+{
+    [TestClass]
+    public class ListTests
+    {
+        private ListOfStudentsRevised studentList;
+        private Student newStudent, newStudent2;
+
+        [TestInitialize]
+        public void SetUp()
+        {
+            newStudent = new Student
+            {
+                StudentID = "X00101941",
+                StudentName = "Daniel Buckley",
+                StudentGender = Gender.Male
+            };
+            newStudent2 = new Student
+            {
+                StudentID = "X1111111",
+                StudentName = "New Student",
+                StudentGender = Gender.Female
+            };
+            studentList = new ListOfStudentsRevised()
+            {
+                ID = 0,
+                LecturerName = "Lecturer",
+                StudentArrayList = new List<Student>()
+            };
+        }
+
+        [TestMethod]
+        public void AddStudentToArrayListTest()
+        {
+            bool result = studentList.AddStudentToList(newStudent);
+            Assert.IsTrue(result);
+        }
+
+        [TestMethod]
+        public void AddTwoStudentsToArrayListTest()
+        {
+            studentList.AddStudentToList(newStudent);
+            studentList.AddStudentToList(newStudent2);
+            Assert.AreEqual(studentList.StudentArrayList.Count, 2);
+        }
+
+        [TestMethod]
+        public void AddStudentToArrayListTestShouldThrowException()
+        {
+            try
+            {
+                studentList.AddStudentToList(newStudent);
+                studentList.AddStudentToList(newStudent);
+                Assert.Fail("Should fail on double studentId");
+            }
+            catch (Exception e)
+            {
+                Assert.IsTrue(e is Exception);
+            }
+        }
+
+        [TestMethod]
+        public void TestIntIndexer()
+        {
+            studentList.AddStudentToList(newStudent);
+            studentList.AddStudentToList(newStudent2);
+            Student s = studentList[0];
+            Assert.AreEqual(s.StudentID, "X00101941");
+            Assert.AreEqual(s.StudentName, "Daniel Buckley");
+            Assert.AreEqual(s.StudentGender, Gender.Male);
+        }
+
+        [TestMethod]
+        public void TestIntIndexerWithException()
+        {
+            try
+            {
+                studentList.AddStudentToList(newStudent);
+                studentList.AddStudentToList(newStudent2);
+                Student s = studentList[5];
+                Assert.Fail("Test should fail!");
+            }
+            catch (IndexOutOfRangeException ind)
+            {
+                Assert.IsTrue(ind is IndexOutOfRangeException);
+            }
+        }
+
+        [TestMethod]
+        public void TestStudentIdIndexer()
+        {
+            studentList.AddStudentToList(newStudent);
+            studentList.AddStudentToList(newStudent2);
+            Student s = studentList["X1111111"];
+            Assert.AreEqual(s.StudentID, "X1111111");
+            Assert.AreEqual(s.StudentName, "New Student");
+            Assert.AreEqual(s.StudentGender, Gender.Female);
+        }
+
+        [TestMethod]
+        public void TestStudentIdIndexerWithException()
+        {
+            try
+            {
+                studentList.AddStudentToList(newStudent);
+                studentList.AddStudentToList(newStudent2);
+                Student s = studentList["X"];
+                Assert.Fail("Test should fail!");
+            }
+            catch (IndexOutOfRangeException ind)
+            {
+                Assert.IsTrue(ind is IndexOutOfRangeException);
+            }
+        }
+    }
+}
